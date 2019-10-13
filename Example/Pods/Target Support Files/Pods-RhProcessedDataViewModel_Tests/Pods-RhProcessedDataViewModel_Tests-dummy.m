df326abda224635bd43bf48eb4975d0afa8955e0
SooHwanCho <chh0107@gmail.com>
2019-10-13 10:58:34 +0900

    delete Unnessesary files

--- Example/Pods/Target Support Files/Pods-RhProcessedDataViewModel_Tests/Pods-RhProcessedDataViewModel_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RhProcessedDataViewModel_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_RhProcessedDataViewModel_Tests
-@end
--- Example/Pods/Target Support Files/RhProcessedDataViewModel/RhProcessedDataViewModel-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RhProcessedDataViewModel : NSObject
-@end
-@implementation PodsDummy_RhProcessedDataViewModel
-@end
